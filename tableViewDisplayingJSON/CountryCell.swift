4bd9197d10271499fdcf1c18d94c6c9789863824
West Kraemer <kraemerwest@gmail.com>
2019-02-15 20:39:22 -0500

    add new vc as a place to click on after user has selected an list item

---
+++ tableViewDisplayingJSON/CountryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CountryCell.swift
+//  tableViewDisplayingJSON
+//
+//  Created by West Kraemer on 2/15/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class countryCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var capitalLabel: UILabel!
+    
+}
+
+
+
+
+
+
+
+
