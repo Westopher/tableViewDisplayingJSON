4aa681df56c39f9f0a686b164d3b163a7b3f81ac
West Kraemer <kraemerwest@gmail.com>
2019-02-15 19:47:50 -0500

    project created

---
+++ tableViewDisplayingJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableViewDisplayingJSON
+//
+//  Created by West Kraemer on 2/15/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
