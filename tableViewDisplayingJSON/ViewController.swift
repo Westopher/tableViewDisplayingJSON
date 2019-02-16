911b615353e3fbde024eb02c11191b695e8cb79f
West Kraemer <kraemerwest@gmail.com>
2019-02-15 21:52:16 -0500

    add json serialization. Need to update models and add model files

--- tableViewDisplayingJSON/ViewController.swift
+++ tableViewDisplayingJSON/ViewController.swift
@@ -16,6 +16,19 @@ class ViewController: UIViewController {
     }
 }
 
+struct DarkSky: Decodable {
+    
+    var currently : String?
+    var daily : String
+    var flags : String
+    var hourly : String
+    var latitude : Float
+    var longitude : Float
+    var minutely : String
+    var offset : Int
+    var timezone : String
+    
+}
 
 var countries = ["USA", "Canada", "Norway", "South Africa", "Germany", "Austria"]
 
@@ -26,7 +39,27 @@ extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! countryCell
-        cell.nameLabel?.text = countries[indexPath.row]
+        //cell.nameLabel?.text = countries[indexPath.row]
+        
+        let jsonUrlString = "https://api.darksky.net/forecast/59c6b6b7efd5c3fc0f617338cfae6c48/37.8267,-122.4233"
+        guard let url = URL(string: jsonUrlString) else {return UITableViewCell()}
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            guard let data = data else {return}
+            
+            do {
+                let darksky = try JSONDecoder().decode(DarkSky.self, from: data)
+                print(darksky.latitude)
+                DispatchQueue.main.async {
+                    cell.nameLabel?.text = String(darksky.latitude)
+                    
+                }
+            } catch let jsonErr {
+                print("Error serializing json", jsonErr)
+            }
+            
+            
+            
+            }.resume()
         return cell
     }
 }
