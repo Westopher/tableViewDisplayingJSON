2280c8d56ff8a96c3447070bbd66a0fbb4812ebb
West Kraemer <kraemerwest@gmail.com>
2019-02-25 18:41:04 -0500

    update api call to be inside viewdidload, rather than inside tableview

--- tableViewDisplayingJSON/Controller/ViewController.swift
+++ tableViewDisplayingJSON/Controller/ViewController.swift
@@ -10,43 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableview: UITableView!
+    var weatherList: [weatherJSON] = []
+    
+    //apikey
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-}
-
-
-
-var countries = ["USA", "Canada", "Norway", "South Africa", "Germany", "Austria"]
-
-extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! countryCell
-        //cell.nameLabel?.text = countries[indexPath.row]
-        
-        let jsonUrlString = "https://api.darksky.net/forecast/59c6b6b7efd5c3fc0f617338cfae6c48/37.8267,-122.4233"
-        guard let url = URL(string: jsonUrlString) else {return UITableViewCell()}
+        let jsonUrlString = "https://api.darksky.net/forecast/59c6b6b7efd5c3fc0f617338cfae6c48/40.7127,-74.0059"
+        guard let url = URL(string: jsonUrlString) else {return}
         URLSession.shared.dataTask(with: url) { (data, response, err) in
             guard let data = data else {return}
             
             do {
                 let JSON = try JSONDecoder().decode(weatherJSON.self, from: data)
-                print(JSON.currently)
+                print(JSON.alerts)
                 DispatchQueue.main.async {
-                    cell.nameLabel?.text = JSON.alerts?.description
+                    self.tableview.reloadData()
                 }
             } catch let jsonErr {
                 print("Error serializing json", jsonErr)
             }
             
-            
-            
             }.resume()
+    }
+}
+
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weatherList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! countryCell
+        
+        
+        //cell.nameLabel?.text = weatherList[indexPath.row]
+        
+        
         return cell
     }
 }
